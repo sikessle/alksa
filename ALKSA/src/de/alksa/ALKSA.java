5cec4faa17c4888f3b6bc92f23ad5aae0812d778
sikessle <sikessle@192.168.1.10>
2015-01-14 14:47:31 +0100

    basic structure

--- ALKSA/src/de/alksa/ALKSA.java
+++ ALKSA/src/de/alksa/ALKSA.java
@@ -4,9 +4,32 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 public class ALKSA {
-	
+
+	private boolean learn;
+
 	public ALKSA() {
 		Injector injector = Guice.createInjector(new DependencyModule());
+		// TODO build togehter all ALKSA modules
 	}
-	
+
+	public void setLearnMode(boolean learn) {
+		this.learn = learn;
+	}
+
+	public void processQuery(String query, String database, String databaseUser) {
+		if (learn) {
+			learnQuery(query, database, databaseUser);
+		} else {
+			checkQuery(query, database, databaseUser);
+		}
+	}
+
+	private void learnQuery(String query, String database, String databaseUser) {
+
+	}
+
+	private void checkQuery(String query, String database, String databaseUser) {
+
+	}
+
 }
