6d4dda78c9ccbcb28db7450964a8f6a47d4c3f56
sikessle <sikessle@192.168.1.10>
2015-01-20 12:23:42 +0100

    cleanup

--- ALKSA/src/de/alksa/parser/impl/RecursiveVisitor.java
+++ ALKSA/src/de/alksa/parser/impl/RecursiveVisitor.java
@@ -8,72 +8,30 @@ import com.foundationdb.sql.parser.Visitable;
  * Must be applied to the different parts of a statement: SelectList, FromList,
  * WhereClause, etc.
  */
+
+// TODO move to Guice via list for OPEN/CLOSED Principle
 public class RecursiveVisitor extends AbstractVisitor {
 
 	@Override
 	public Visitable visit(Visitable node) throws StandardException {
 
-		visitConstants(node);
-		visitColumnNames(node);
-		visitFunctions(node);
-		visitCalculations(node);
+		addTokensFromVisitor(node, new ConstantVisitor());
+		addTokensFromVisitor(node, new ColumnNameVisitor());
+		addTokensFromVisitor(node, new FunctionVisitor());
+		addTokensFromVisitor(node, new CalculationVisitor());
+		addTokensFromVisitor(node, new FromBaseTableVisitor());
+		addTokensFromVisitor(node, new FromJoinVisitor());
+		addTokensFromVisitor(node, new FilterVisitor());
+		
 		// TODO add other types like nested expression, ..
-
-		visitFromBaseTables(node);
-		visitJoins(node);
-		visitFilters(node);
 
 		return node;
 	}
 
-	private void visitConstants(Visitable node) throws StandardException {
-		AbstractVisitor constantVisitor = new ConstantVisitor();
-		node.accept(constantVisitor);
-
-		addAllTokens(constantVisitor.getTokens());
-	}
-
-	private void visitColumnNames(Visitable node) throws StandardException {
-
-		AbstractVisitor columnNameVisitor = new ColumnNameVisitor();
-		node.accept(columnNameVisitor);
-
-		addAllTokens(columnNameVisitor.getTokens());
-	}
-
-	private void visitFunctions(Visitable node) throws StandardException {
-		AbstractVisitor functionColumnVisitor = new FunctionVisitor();
-		node.accept(functionColumnVisitor);
-
-		addAllTokens(functionColumnVisitor.getTokens());
-	}
-
-	private void visitCalculations(Visitable node) throws StandardException {
-		AbstractVisitor calculationVisitor = new CalculationVisitor();
-		node.accept(calculationVisitor);
-
-		addAllTokens(calculationVisitor.getTokens());
-	}
-
-	private void visitFromBaseTables(Visitable node) throws StandardException {
-		AbstractVisitor fromTableVisitor = new FromBaseTableVisitor();
-		node.accept(fromTableVisitor);
-
-		addAllTokens(fromTableVisitor.getTokens());
-	}
-
-	private void visitJoins(Visitable node) throws StandardException {
-		AbstractVisitor joinVisitor = new FromJoinVisitor();
-		node.accept(joinVisitor);
-
-		addAllTokens(joinVisitor.getTokens());
-	}
-
-	private void visitFilters(Visitable node) throws StandardException {
-		AbstractVisitor filterVisitor = new FilterVisitor();
-		node.accept(filterVisitor);
-
-		addAllTokens(filterVisitor.getTokens());
+	private void addTokensFromVisitor(Visitable node,
+			AbstractVisitor visitor) throws StandardException {
+		node.accept(visitor);
+		addAllTokens(visitor.getTokens());
 	}
 
 }
