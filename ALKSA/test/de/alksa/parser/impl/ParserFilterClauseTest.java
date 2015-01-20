7bceeda80e72c8cd233aac238a567d3fe1bcdd43
sikessle <sikessle@192.168.1.10>
2015-01-20 14:19:54 +0100

    more tests for nested conditions

--- ALKSA/test/de/alksa/parser/impl/ParserFilterClauseTest.java
+++ ALKSA/test/de/alksa/parser/impl/ParserFilterClauseTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
@@ -13,6 +14,7 @@ import de.alksa.token.BinaryLogicalFilterToken;
 import de.alksa.token.ColumnNameToken;
 import de.alksa.token.ComparisonFilterToken;
 import de.alksa.token.ConstantValueToken;
+import de.alksa.token.FunctionToken;
 import de.alksa.token.Token;
 import de.alksa.token.UnaryLogicalFilterToken;
 import de.alksa.token.WhereClauseToken;
@@ -73,7 +75,7 @@ public class ParserFilterClauseTest {
 				ComparisonFilterToken.Type.EQUAL, new ConstantValueToken(2));
 		Token cEqualsA = new ComparisonFilterToken(new ColumnNameToken("c"),
 				ComparisonFilterToken.Type.EQUAL, new ColumnNameToken("a"));
-		
+
 		Token andToken = new BinaryLogicalFilterToken(
 				BinaryLogicalFilterToken.Type.AND, aEquals2,
 				new ColumnNameToken("b"));
@@ -97,26 +99,28 @@ public class ParserFilterClauseTest {
 			}
 		}
 	}
-	
+
 	@Test
 	public void testComplicatedComparison() {
-		// (a=2 AND b) OR NOT(c=a)
-		fail("not implemented");
-		String sql = "SELECT x FROM y WHERE a = 2 AND b OR NOT c = a";
+		// (a >= 2) OR (b < 3 AND c = ABS(2))
+		String sql = "SELECT x FROM y WHERE a >= 2 OR b < 3 AND c = ABS(2)";
 
-		Token aEquals2 = new ComparisonFilterToken(new ColumnNameToken("a"),
-				ComparisonFilterToken.Type.EQUAL, new ConstantValueToken(2));
-		Token cEqualsA = new ComparisonFilterToken(new ColumnNameToken("c"),
-				ComparisonFilterToken.Type.EQUAL, new ColumnNameToken("a"));
-		
+		Token aGreaterEqual2 = new ComparisonFilterToken(new ColumnNameToken(
+				"a"), ComparisonFilterToken.Type.GREATER_EQUAL,
+				new ConstantValueToken(2));
+		Token bLess3 = new ComparisonFilterToken(new ColumnNameToken("b"),
+				ComparisonFilterToken.Type.LESS, new ConstantValueToken(3));
+		List<Token> parameters = new ArrayList<>();
+		parameters.add(new ConstantValueToken(2));
+		Token cEqualAbs2 = new ComparisonFilterToken(new ColumnNameToken("c"),
+				ComparisonFilterToken.Type.EQUAL, new FunctionToken("ABS",
+						parameters));
+
 		Token andToken = new BinaryLogicalFilterToken(
-				BinaryLogicalFilterToken.Type.AND, aEquals2,
-				new ColumnNameToken("b"));
-		Token notToken = new UnaryLogicalFilterToken(
-				UnaryLogicalFilterToken.Type.NOT, cEqualsA);
-
+				BinaryLogicalFilterToken.Type.AND, bLess3, cEqualAbs2);
+		
 		Token expected = new BinaryLogicalFilterToken(
-				BinaryLogicalFilterToken.Type.OR, andToken, notToken);
+				BinaryLogicalFilterToken.Type.OR, aGreaterEqual2, andToken);
 
 		List<Token> tokens = parser.parse(sql);
 
@@ -128,6 +132,9 @@ public class ParserFilterClauseTest {
 				Token actual = ((WhereClauseToken) token).getChildren().get(0);
 
 				assertEquals(expected, actual);
+				
+				System.out.println("actual: " + actual);
+				System.out.println("expected: " + expected);
 
 			}
 		}
