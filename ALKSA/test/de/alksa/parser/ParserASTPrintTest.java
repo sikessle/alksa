f15fb371d8d57c1cd9459af179c1e779e9eccc7a
sikessle <sikessle@192.168.1.10>
2015-01-20 14:50:52 +0100

    renamed and made parameterized

---
+++ ALKSA/test/de/alksa/parser/ParserASTPrintTest.java
@@ -0,0 +1,52 @@
+package de.alksa.parser;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.foundationdb.sql.StandardException;
+import com.foundationdb.sql.parser.SQLParser;
+import com.foundationdb.sql.parser.StatementNode;
+
+@RunWith(Parameterized.class)
+public class ParserASTPrintTest {
+
+	private String query;
+	private static final String DELIMITER = "++++++++++++++++++++++++++++++++++++++++++++++++++++";
+
+	public ParserASTPrintTest(String query) {
+		this.query = query;
+	}
+
+	@Parameters
+	public static Collection<Object[]> generateData() {
+		return Arrays
+				.asList(new Object[][] {
+						{ "SELECT ABS(x), AVG(y) FROM y WHERE a AND b OR NOT c OR col = 'b'" },
+						{ "SELECT x FROM b" } });
+	}
+
+	@Test
+	public void parseExample() {
+		SQLParser sqlParser = new SQLParser();
+		try {
+			parseString(query, sqlParser);
+		} catch (StandardException e) {
+			System.err.println("Failed parsing");
+			e.printStackTrace();
+		}
+
+	}
+
+	private void parseString(String query, SQLParser sqlParser)
+			throws StandardException {
+		StatementNode statement = sqlParser.parseStatement(query);
+		System.out.println(query);
+		statement.treePrint();
+		System.out.println(DELIMITER);
+	}
+}
