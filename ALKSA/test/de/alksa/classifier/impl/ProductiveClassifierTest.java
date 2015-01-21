f494ba9ca412a98551a823e9b49069c6044af015
sikessle <sikessle@192.168.1.10>
2015-01-21 15:53:45 +0100

    test for productive mode

--- ALKSA/test/de/alksa/classifier/impl/ProductiveClassifierTest.java
+++ ALKSA/test/de/alksa/classifier/impl/ProductiveClassifierTest.java
@@ -3,24 +3,28 @@ package de.alksa.classifier.impl;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.alksa.log.Logger;
 import de.alksa.querystorage.Query;
 import de.alksa.querystorage.QueryStorage;
 
 import static org.junit.Assert.assertTrue;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
-public class LearningClassifierTest extends StateClassifierTest {
+public class ProductiveClassifierTest extends StateClassifierTest {
 
-	private LearningClassifier classifier;
+	private ProductiveClassifier classifier;
 	private QueryStorage queryStorageMock;
+	private Logger loggerMock;
 	private Query query;
 
 	@Before
 	public void setUp() throws Exception {
 		queryStorageMock = mock(QueryStorage.class);
+		loggerMock = mock(Logger.class);
 
-		classifier = new LearningClassifier(queryStorageMock);
+		classifier = new ProductiveClassifier(queryStorageMock, loggerMock);
 		query = createQuery("SELECT name FROM users WHERE x = 2", "local",
 				"tester");
 	}
@@ -28,7 +32,7 @@ public class LearningClassifierTest extends StateClassifierTest {
 	@Test
 	public void testAccept() {
 		assertTrue(classifier.accept(query));
-		verify(queryStorageMock).write(query);
+		verify(queryStorageMock).read();
 	}
 
 	@Test(expected = NullPointerException.class)
