aebb9108be97fec6a0e12f0b0bda7dfbb4487169
sikessle <sikessle@192.168.1.10>
2015-01-21 15:56:13 +0100

    hashcode and equals

--- ALKSA/src/de/alksa/log/impl/AttackLogEntry.java
+++ ALKSA/src/de/alksa/log/impl/AttackLogEntry.java
@@ -54,4 +54,71 @@ public class AttackLogEntry implements LogEntry {
 		return timestamp;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((database == null) ? 0 : database.hashCode());
+		result = prime * result
+				+ ((databaseUser == null) ? 0 : databaseUser.hashCode());
+		result = prime * result + ((query == null) ? 0 : query.hashCode());
+		result = prime * result
+				+ ((timestamp == null) ? 0 : timestamp.hashCode());
+		result = prime * result
+				+ ((violation == null) ? 0 : violation.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		AttackLogEntry other = (AttackLogEntry) obj;
+
+		if (database == null) {
+			if (other.database != null) {
+				return false;
+			}
+		} else if (!database.equals(other.database)) {
+			return false;
+		}
+		if (databaseUser == null) {
+			if (other.databaseUser != null) {
+				return false;
+			}
+		} else if (!databaseUser.equals(other.databaseUser)) {
+			return false;
+		}
+		if (query == null) {
+			if (other.query != null) {
+				return false;
+			}
+		} else if (!query.equals(other.query)) {
+			return false;
+		}
+		if (timestamp == null) {
+			if (other.timestamp != null) {
+				return false;
+			}
+		} else if (!timestamp.equals(other.timestamp)) {
+			return false;
+		}
+		if (violation == null) {
+			if (other.violation != null) {
+				return false;
+			}
+		} else if (!violation.equals(other.violation)) {
+			return false;
+		}
+		return true;
+	}
+
 }
